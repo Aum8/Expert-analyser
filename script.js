c26d0030a225e1625876be9cbd41d586d7a4637a
Aum <92933416+Aum8@users.noreply.github.com>
2023-06-08 15:32:46 +0530

    Update script.js
    modified to use dictionary

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
 function analyzeText() {
   var text = document.getElementById("textInput").value;
-  var level = calculateExpertiseLevel(text);
+  var score = calculateWordQualityScore(text);
+  var level = determineExpertiseLevel(score);
   var output = document.getElementById("output");
 
   output.innerHTML = "Expertise level: " + level;
 }
 
-function calculateExpertiseLevel(text) {
-  // Your logic to determine the expertise level goes here
-  // You can use any algorithm or criteria you prefer
+function calculateWordQualityScore(text) {
+  // Define a list of words and their corresponding quality scores
+  var wordScores = {
+    good: 2,
+    excellent: 3,
+    bad: -1,
+    poor: -2,
+    average: 0
+    // Add more words and scores as needed
+  };
 
-  // Example logic:
-  var wordCount = text.trim().split(" ").length;
-  if (wordCount < 10) {
-    return "Beginner";
-  } else if (wordCount < 50) {
+  var words = text.trim().toLowerCase().split(/\s+/);
+  var score = 0;
+
+  // Iterate through the words and sum up the scores
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i];
+    if (word in wordScores) {
+      score += wordScores[word];
+    }
+  }
+
+  return score;
+}
+
+function determineExpertiseLevel(score) {
+  if (score >= 5) {
+    return "Expert";
+  } else if (score >= 0) {
     return "Intermediate";
   } else {
-    return "Expert";
+    return "Beginner";
   }
 }
