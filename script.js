c33a817c0516f58e649dd644744726db72a3000d
Aum <92933416+Aum8@users.noreply.github.com>
2023-06-10 22:47:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-function analyzeText() {
-  var text = document.getElementById("textInput").value;
-  var level = calculateExpertiseLevel(text);
-  var output = document.getElementById("output");
+function determineExpertiseLevel(text) {
+  var wordScores = {
+    // Define your dictionary of words with values ranging between -1 and 1
+    good: 0.8,
+    excellent: 1,
+    bad: -0.8,
+    poor: -1,
+    average: 0
+    // Add more words and scores as needed
+  };
 
-  output.innerHTML = "Expertise level: " + level;
-}
+  var words = text.trim().toLowerCase().split(/\s+/);
+  var score = 0;
 
-function calculateExpertiseLevel(text) {
-  // Your logic to determine the expertise level goes here
-  // You can use any algorithm or criteria you prefer
+  // Calculate the overall score based on the words in the text
+  for (var i = 0; i < words.length; i++) {
+    var word = words[i];
+    if (word in wordScores) {
+      score += wordScores[word];
+    }
+  }
 
-  // Example logic:
-  var wordCount = text.trim().split(" ").length;
-  if (wordCount < 10) {
-    return "Beginner";
-  } else if (wordCount < 50) {
+  // Determine the expertise level based on the overall score
+  if (score >= 0.8) {
+    return "Expert";
+  } else if (score >= 0.2) {
     return "Intermediate";
   } else {
-    return "Expert";
+    return "Beginner";
   }
 }
